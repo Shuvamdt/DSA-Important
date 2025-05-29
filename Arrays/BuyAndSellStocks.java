a2104fe0dc0eeed7ec05db72f9193479120f5473
Shuvamdt <shuvamdt1230@gmail.com>
2025-05-29 13:06:09 +0530

    Buy and Sell Stocks / Array

---
+++ Arrays/BuyAndSellStocks.java
@@ -0,0 +1,15 @@
+public class BuyAndSellStocks{
+    public static void main(String[] args) {
+        int prices[]={7, 1, 5, 3, 6, 4};
+        int bp=Integer.MAX_VALUE,sp=0,maxProfit=0;
+        for(int i=0 ; i<prices.length ; i++){
+            if(bp<prices[i]){
+                int profit = prices[i]-bp;
+                maxProfit=Math.max(maxProfit, profit);
+            }else{
+                bp=prices[i];
+            }
+        }
+        System.out.println("The maximum profit is: "+maxProfit);
+    }
+}
